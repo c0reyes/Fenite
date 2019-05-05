c8e20a27645b57fc41cca7616dd52558bf8a63c4
Conrado Reyes <coreyes@gmail.com>
2019-05-04 21:13:14 -0400

    bleh...

--- src/fenite.pl
+++ src/fenite.pl
@@ -132,18 +132,32 @@ sub _loop {
 
 sub _process {
     my $msg = shift;
-	my $u = $msg->{from}{username};
-    my $id = $msg->{from}{id};
 
-    if(!$u) {
-        $u = $msg->{from}{first_name};
+	my $username = $msg->{from}{username};
+    my $firstname = $msg->{from}{first_name};
+    my $id = $msg->{from}{id};
+    my $tme = "[" . encode("utf8", $firstname) . "](tg://user?id=" . $id. ")";
+
+    my $username_reply;
+    my $firstname_reply;
+    my $id_reply;
+    my $tme_reply;
+
+    if($msg->{reply_to_message}) {
+        $username_reply = $msg->{reply_to_message}{from}{username};
+        $firstname_reply = $msg->{reply_to_message}{from}{first_name};
+        $id_reply = $msg->{reply_to_message}{from}{id};
+        $tme_reply = "[" . encode("utf8", $firstname_reply) . "](tg://user?id=" . $id_reply. ")";
     }
     
     my $text = $msg->{text};
-    $text = $msg->{caption} if($msg->{caption});
+    $text .= $msg->{caption};
+    $text .= $msg->{reply_to_message}{text} if $text !~ /^\//;
+    $text .= $msg->{reply_to_message}{caption} if $text !~ /^\//;
 
-    if($msg->{voice}) {
-        my $file_id = $msg->{voice}{file_id};
+    # Voice to Text
+    if($msg->{voice} || ($msg->{reply_to_message}{voice} && $text !~ /^\//)) {
+        my $file_id = $msg->{voice}{file_id} || $msg->{reply_to_message}{voice}{file_id};
 
         my $u = "https://api.telegram.org/bot$bot->{config}{token}/getFile?file_id=$file_id";
         my $r = get($u);
@@ -163,44 +177,55 @@ sub _process {
 
         my $operation = $speech->syncrecognize();
 
-        $text = (split(/\"/,JSON->new->utf8->encode($operation->results)))[7];
-        #$bot->_log($text);
+        $text .= (split(/\"/,JSON->new->utf8->encode($operation->results)))[7];
 
-        unlink($file_id . ".ogg");
+        unlink("$file_id.ogg");
     }
 
+    # Read photo from LaTumba
+    if(($msg->{photo} || ($msg->{reply_to_message}{photo} && $text !~ /^\//)) && $msg->{chat}{id} eq "-1001071751643") {
+        my $file_id = $msg->{photo}{file_id} || $msg->{reply_to_message}{photo}{file_id};
+
+        my $u = "https://api.telegram.org/bot$bot->{config}{token}/getFile?file_id=$file_id";
+        my $r = get($u);
+        my $json = JSON->new->utf8->decode($r);
+
+        my $f = $json->{result}->{file_path};
+
+        $u = "https://api.telegram.org/file/bot$bot->{config}{token}/$f";
+        system("wget -q -O $file_id.jpg $u");
+        $text .= qx/tesseract $file_id.jpg -/;
+        unlink("$file_id.jpg");
+    } 
+
+    # Responder a los MMGS
     if($text =~ /$regex/i) {
-        # Responder a los MMGS
         my $m = $mmgs[rand @mmgs];
         chomp($m);
 
-        _send($msg, $m, "\@" . $u);
+        _send($msg, $m, $tme);
 
-        my $nn = $u;
-        $nn =~ s/\@|\s//g;
-        mmg($msg->{chat}{id}, $nn, $id);
+        $username =~ s/\@|\s//g;
+        mmg($msg->{chat}{id}, $username, $id, $firstname, $msg->{chat}{title});
         return;
     }
 
     # Responder a di lo tuyo
     if($text =~ /di lo tuyo|Say your thing/i) {
-        my $fu = $msg->{reply_to_message}{from}{username} ? 
-                   $msg->{reply_to_message}{from}{username} : 
-                   $msg->{reply_to_message}{from}{first_name};
-
-        $fu = $u if(!$fu || $fu eq "fenite_bot");
-        $fu = $u if($u eq "ChargingStar");
-
         my $m = $mmgs[rand @mmgs];
         chomp($m);
         
-        _send($msg, $m, "\@" . $fu);
+        if((!$firstname_reply && !$username_reply) || $username_reply eq "fenite_bot") {
+            _send($msg, $m, $tme);
+        }else{
+            _send($msg, $m, $tme_reply);
+        }
 
         return;
     }
 
     # Reload
-    if($text =~ /reload/i && $u =~ /$op/i) {
+    if($text =~ /reload/i && $username =~ /$op/i) {
         _load();
         sendmsg($msg->{chat}{id}, "reload...");
         return;
@@ -213,7 +238,7 @@ sub _process {
     }else{
         # Responder texto
         foreach my $key (keys %resp) {
-            if($msg->{text} =~ /$key/i) {
+            if($msg->{text} =~ /$key[\s\n\r?!\.]|$key$/i) {
                 _send($msg, $resp{$key});
                 last;
             }
@@ -232,37 +257,50 @@ sub _send {
         $bot->sendDocument([
             chat_id => $msg->{chat}{id},
             document => $t[0],
-            caption => $codename
+            caption => $codename,
+            parse_mode => 'Markdown'
         ]);
     }elsif($t[1] eq "photo") {
         $bot->sendPhoto([
             chat_id => $msg->{chat}{id},
             photo => $t[0],
-            caption => $codename
+            caption => $codename,
+            parse_mode => 'Markdown'
         ]);
     }elsif($t[1] eq "voice") {
         $bot->sendVoice([
             chat_id => $msg->{chat}{id},
             voice => $t[0],
-            caption => $codename
+            caption => $codename,
+            parse_mode => 'Markdown'
         ]);
     }elsif($t[1] eq "audio") {
         $bot->sendAudio([
             chat_id => $msg->{chat}{id},
             audio => $t[0],
-            caption => $codename
+            caption => $codename,
+            parse_mode => 'Markdown'
         ]);
     }elsif($t[1] eq "video") {
         $bot->sendVideo([
             chat_id => $msg->{chat}{id},
             video => $t[0],
-            caption => $codename
+            caption => $codename,
+            parse_mode => 'Markdown'
+        ]);
+    }elsif($t[1] eq "animation") {
+        $bot->sendVideo([
+            chat_id => $msg->{chat}{id},
+            animation => $t[0],
+            caption => $codename,
+            parse_mode => 'Markdown'
         ]);
     }elsif($t[1] eq "sticker") {
         if($codename) {
             $bot->sendMessage([
                 chat_id => $msg->{chat}{id},
-                text => $codename
+                text => $codename,
+                parse_mode => 'Markdown'
             ]);
         }
         $bot->sendSticker([
@@ -279,6 +317,11 @@ sub mmg {
     my $chatid = shift;
     my $codename = shift;
     my $id = shift;
+    my $firstname = shift;
+    my $chat = shift;
+
+    $codename = "*NA*" if(!$codename);
+    $chat = "private" if(!$chat);
 
     my $year = strftime "%Y", localtime;
 
@@ -292,8 +335,8 @@ sub mmg {
 
     if($ret[0] > 0) {
         # UPDATE CON ID
-        $qry = "update fenite_mmg set count = count + 1, codename = ? where id = ? and chatid = ? and year = ?";
-        @param_qry = ($codename, $id, $chatid, $year);
+        $qry = "update fenite_mmg set count = count + 1, codename = ?, firstname = ?, chat = ? where id = ? and chatid = ? and year = ?";
+        @param_qry = ($codename, $firstname, $chat, $id, $chatid, $year);
     }else{
         @param = ();
         @ret = ();
@@ -304,12 +347,12 @@ sub mmg {
 
         if($ret[0] > 0) {
             # Update
-            $qry = "update fenite_mmg set count = count + 1, id = ? where codename = ? and chatid = ? and year = ?";
-            @param_qry = ($id, $codename, $chatid, $year);
+            $qry = "update fenite_mmg set count = count + 1, id = ?, firstname = ?, chat = ? where codename = ? and chatid = ? and year = ?";
+            @param_qry = ($id, $firstname, $chat, $codename, $chatid, $year);
         }else{
             # Insert
-            $qry = "insert into fenite_mmg (codename, chatid, count, year, id) values (?,?,1,?,?)";
-            @param_qry = ($codename, $chatid, $year, $id);
+            $qry = "insert into fenite_mmg (codename, chatid, count, year, id, firstname, chat) values (?,?,1,?,?,?,?)";
+            @param_qry = ($codename, $chatid, $year, $id, $firstname, $chat);
         }
     }
 
