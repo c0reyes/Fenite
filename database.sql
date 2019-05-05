f69f1109b31538740109e132fb5da8afd0d31cd3
Conrado Reyes <coreyes@gmail.com>
2019-05-04 21:24:09 -0400

    Database

--- database.sql
+++ database.sql
@@ -1,5 +1,6 @@
 create table fenite_frases (
-	frase text not null
+	frase text not null, 
+	type varchar(20)
 );
 
 create table fenite_molestar (
@@ -13,7 +14,11 @@ create table fenite_op (
 create table fenite_mmg (
 	chatid char(50) not null,
 	codename char(100) not null,
-	count int not null
+	count int not null,
+	year char(4), 
+	id varchar(100), 
+	firstname varchar(100), 
+	chat varchar(100)
 );
 
 create table fenite_regex (
